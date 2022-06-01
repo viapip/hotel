eef0d7a49b7428b73254dbb90f7394714a69808d
Nikita S <nicksas@MacBook-Pro-Nikita.local>
2022-06-01 18:34:11 +0300

    Change title name for admin

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -51,7 +51,7 @@
         <a href="/" target="_blank" class="brand-link">
             <img src="{{ asset('/assets/admin/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
                  class="brand-image img-circle elevation-3" style="opacity: .8">
-            <span class="brand-text font-weight-light">На сайт</span>
+            <span class="brand-text font-weight-light">Go to the site</span>
         </a>
 
         <!-- Sidebar -->
