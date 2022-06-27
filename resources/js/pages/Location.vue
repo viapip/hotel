30992b68b332507c7dd3290454f82e09a1c3d0a9
Alec <starcev.sash@gmail.com>
2022-06-27 17:47:31 +0300

    added rooms page

--- resources/js/pages/Location.vue
+++ resources/js/pages/Location.vue
@@ -1,9 +1,9 @@
 <template>
 
-    <header class="header" :style="[`background: url(${location?.banner}) center/cover no-repeat`]">
+    <header class="header" :style="[`background: url(${data?.banner}) center/cover no-repeat`]">
         <div class="container">
             <div class="header__wrapper">
-                <h1>{{ location?.title }}</h1>
+                <h1>{{ data?.title }}</h1>
                 <div class="breadcrumb">
                     <a href="#" class="breadcrumb__item">Home</a>
                     <a href="#" class="breadcrumb__item">Location</a>
@@ -17,7 +17,7 @@
         <section class="location">
             <div class="container container--sm">
                 <div class="location__text">
-                    {{ location?.description }}
+                    {{ data?.description }}
                 </div>
                 <div class="location__get">Get directions</div>
             </div>
@@ -28,26 +28,24 @@
 
 <script>
 import DiscoverSlider from "../components/DiscoverSlider";
+import {useFetchData} from "../hooks/useFetchData";
 export default {
     name: "Location",
     components: {DiscoverSlider},
+    setup(props) {
+        const {data, isLoading} = useFetchData('/api/location-page');
+        return {
+            data, isLoading
+        }
+    },
     data() {
         return {
             location: null
         }
     },
     methods: {
-        async fetchDataLocation() {
-            try {
-                const response = await fetch(process.env.MIX_HOST_API + '/api/location-page')
-                this.location = await response.json();
-            } catch (e) {
-                console.log(e)
-            }
-        },
     },
     mounted() {
-        this.fetchDataLocation();
     },
 }
 </script>
