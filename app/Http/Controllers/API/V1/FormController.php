f8063a1de37d2a9f4514201e3b81121fea20ad52
Nikita S <nicksas@MacBook-Pro-Nikita.local>
2022-06-29 15:05:01 +0300

    Fix bug mail

--- app/Http/Controllers/API/V1/FormController.php
+++ app/Http/Controllers/API/V1/FormController.php
@@ -2,13 +2,9 @@
 
 namespace App\Http\Controllers\API\V1;
 
-use App\Http\Controllers\Admin\RoomsPageController;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\FormUserRequest;
 use App\Mail\Form;
-use App\Models\Discover;
-use App\Models\Room;
-use App\Models\RoomsPage;
 use Illuminate\Support\Facades\Mail;
 
 class FormController extends Controller
@@ -16,7 +12,7 @@ class FormController extends Controller
 
     public function default(FormUserRequest $request)
     {
-        return Mail::to(config('mail.to.address'))->send(new Form($request->all()));
+        return Mail::to(config('email.to.address'))->send(new Form($request->all()));
     }
 
 
--- config/mail.php
+++ config/mail.php
@@ -96,10 +96,6 @@ return [
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
-    'to' => [
-        'address' => env('MAIL_TO_ADDRESS', 'hello@example.com'),
-    ],
-
     /*
     |--------------------------------------------------------------------------
     | Markdown Mail Settings
