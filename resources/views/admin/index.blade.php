26a377de6d3f38d24f08b74187a0d8e10434eede
Alec <starcev.sash@gmail.com>
2022-07-05 15:01:21 +0300

    test form

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,5 +3,51 @@
 @section('content')
 
 
+    <form id="test123" action="/admin/image">
+        <input name="file" type="file">
+        <button>оптправка</button>
+    </form>
+
+    <script>
+
+        const getToken = (name) => {
+            let matches = document.cookie.match(new RegExp(
+                "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+            ));
+            return matches ? decodeURIComponent(matches[1]) : undefined;
+        }
+
+        const fetchData = async (data, url) => {
+            try {
+                const response = fetch(url, {
+                    method: "POST",
+                    headers: {
+                        'X-CSRF-TOKEN':  getToken('XSRF-TOKEN')
+                    },
+                    body: data,
+                })
+
+            } catch (e) {
+                console.log(e)
+                return false;
+            }
+        }
+        document.querySelector('#test123')
+            .addEventListener('submit', async (e) => {
+                e.preventDefault()
+                const target = e.currentTarget
+                target.querySelector('button').innerText = 'грузим'
+                const formData = new FormData(target);
+                const url = target.getAttribute('action')
+                const response = await fetchData(formData, url)
+                if(!response) {
+                    target.querySelector('button').innerText = 'неудачно'
+                    return false;
+                }
+
+                target.querySelector('button').innerText = 'отправилось'
+
+            })
+    </script>
 
 @endsection
