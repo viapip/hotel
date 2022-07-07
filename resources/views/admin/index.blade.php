097120ea19f7be190927eea386c5e821e554055a
Alec <starcev.sash@gmail.com>
2022-07-07 13:04:24 +0300

    check delete

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -13,17 +13,17 @@
         <button>удалить</button>
     </form>
 
-{{--    <input style="width: 100%" id="dragZoneInput" type="text" value="https://klike.net/uploads/posts/2019-03/medium/1551511866_11.jpg,https://klike.net/uploads/posts/2019-03/1551511801_1.jpg,https://klike.net/uploads/posts/2019-03/medium/1551511784_4.jpg,https://klike.net/uploads/posts/2019-03/1551511774_9.jpg">--}}
+    {{--    <input style="width: 100%" id="dragZoneInput" type="text" value="https://klike.net/uploads/posts/2019-03/medium/1551511866_11.jpg,https://klike.net/uploads/posts/2019-03/1551511801_1.jpg,https://klike.net/uploads/posts/2019-03/medium/1551511784_4.jpg,https://klike.net/uploads/posts/2019-03/1551511774_9.jpg">--}}
 
-{{--    <button id="uploadZone" class="zoneUpload">--}}
-{{--        Загрузить--}}
-{{--    </button>--}}
-{{--    <ul id="dragZone" class="drag-zone">--}}
-{{--    </ul>--}}
+    {{--    <button id="uploadZone" class="zoneUpload">--}}
+    {{--        Загрузить--}}
+    {{--    </button>--}}
+    {{--    <ul id="dragZone" class="drag-zone">--}}
+    {{--    </ul>--}}
 
-{{--    <script src="{{ 'adminJS/uploaderDragZone.js' }}">--}}
+    {{--    <script src="{{ 'adminJS/uploaderDragZone.js' }}">--}}
 
-{{--    </script>--}}
+    {{--    </script>--}}
     <script>
 
         // const test = new DragDrop({zone: '#dragZone', input: '#dragZoneInput', uploadZone: '#uploadZone'})
@@ -42,7 +42,9 @@
                 const response = await fetch(url, {
                     method: method,
                     headers: {
-                        'X-XSRF-TOKEN':  getToken('XSRF-TOKEN')
+                        'X-XSRF-TOKEN': getToken('XSRF-TOKEN'),
+                        // !!!!!!!!!!!!!!!!!!!!!!!!!!!
+                        'Content-Type': 'application/json'
                     },
                     body: data,
                 })
@@ -60,33 +62,47 @@
                 return false;
             }
         }
+        const handlerFormData = (data, method) => {
+            if (method === "POST") return new FormData(data);
+
+            const obj = [data.querySelector('input').value]
+
+            // const formData = new FormData();
+            // formData.append('images', JSON.stringify(obj))
+            // console.log(Object.fromEntries(formData))
+            // return formData
+            return JSON.stringify({images: obj})
+        }
 
         document.querySelectorAll('form').forEach(form => {
 
             form
-            .addEventListener('submit', async (e) => {
-                e.preventDefault()
-                const target = e.currentTarget
-                target.querySelector('button').innerText = 'грузим'
-                const method = target.dataset.method
-                const formData = new FormData(target);
-                const url = target.getAttribute('action')
-                const response = await fetchData(formData, url, method)
-                if(!response) {
-                    target.querySelector('button').innerText = 'неудачно'
-                    return false;
-                }
+                .addEventListener('submit', async (e) => {
+                    e.preventDefault()
+                    const target = e.currentTarget
+                    target.querySelector('button').innerText = 'грузим'
+                    const method = target.dataset.method
+                    console.log(target, 'target')
+                    const formData = handlerFormData(target, method)
+                    const url = target.getAttribute('action')
+                    console.log(formData)
+                    const response = await fetchData(formData, url, method)
+                    if (!response) {
+                        target.querySelector('button').innerText = 'неудачно'
+                        return false;
+                    }
 
-                target.querySelector('button').innerText = 'отправилось'
+                    target.querySelector('button').innerText = 'отправилось'
 
-            })
+                })
         })
     </script>
 
     <style>
         .container {
             max-width: 1000px;
-            margin: auto; }
+            margin: auto;
+        }
 
         .zoneUpload {
             width: 100%;
@@ -97,16 +113,21 @@
             box-shadow: none;
             margin-bottom: 30px;
             border-radius: 10px;
-            color: white; }
+            color: white;
+        }
 
         .drag-zone {
             cursor: pointer;
             list-style-type: none;
             display: flex;
             flex-wrap: wrap;
-            transition: background-color 0.2s linear; }
+            transition: background-color 0.2s linear;
+        }
+
         .drag-zone.zone-in {
-            background-color: antiquewhite; }
+            background-color: antiquewhite;
+        }
+
         .drag-zone__close {
             position: absolute;
             top: 15px;
@@ -115,12 +136,18 @@
             border-radius: 5px;
             cursor: pointer;
             padding: 3px;
-            display: flex; }
+            display: flex;
+        }
+
         .drag-zone__close svg {
             height: 15px;
-            width: 15px; }
+            width: 15px;
+        }
+
         .drag-zone__close:hover {
-            background: white; }
+            background: white;
+        }
+
         .drag-zone__item {
             width: 100%;
             max-width: 10%;
@@ -131,29 +158,43 @@
             display: block;
             position: relative;
             padding-left: 5px;
-            padding-right: 5px; }
+            padding-right: 5px;
+        }
+
         .drag-zone__item.current-drag {
             opacity: 0.4;
-            cursor: grabbing; }
+            cursor: grabbing;
+        }
+
         .drag-zone__item.over {
             transform: scale(1.1);
             border-left: 2px solid blue;
-            cursor: grabbing; }
+            cursor: grabbing;
+        }
+
         .drag-zone__item.last-element {
             background: transparent;
-            cursor: auto; }
+            cursor: auto;
+        }
+
         .drag-zone__item:hover:active {
-            cursor: grabbing; }
+            cursor: grabbing;
+        }
+
         .drag-zone__wrapper-img {
             position: relative;
             width: 100%;
             color: black;
             border-radius: 30px;
-            overflow: hidden; }
+            overflow: hidden;
+        }
+
         .drag-zone__wrapper-img:before {
             display: block;
             content: "";
-            padding-bottom: 100%; }
+            padding-bottom: 100%;
+        }
+
         .drag-zone__img {
             position: absolute;
             height: 100%;
@@ -162,15 +203,10 @@
             left: 0;
             background-position: center;
             background-size: cover;
-            background-repeat: no-repeat; }
+            background-repeat: no-repeat;
+        }
 
         /*# sourceMappingURL=drag-zone.css.map */
-
-
-
-
-
-
 
 
     </style>
